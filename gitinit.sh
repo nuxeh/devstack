775a96307ca5c2ac0a2ff4ce9fbfc3df19fcfd58
Edward Cragg <edward.crag@codethink.co.uk>
2015-04-06 12:39:55 +0100

    Add gitinit.sh

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global alias.ct commit
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global user.name "Edward Cragg"
+git config --global user.email "edward.crag@codethink.co.uk"
